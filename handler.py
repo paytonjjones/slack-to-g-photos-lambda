d824fff7d51671752fb30b1baa53b322426f92dd
Payton J. Jones <payton-jones@pluralsight.com>
2021-12-31 15:26:18 -0800

    send photos to frame via email

--- handler.py
+++ handler.py
@@ -1,8 +1,9 @@
 import json
 import os
 from dotenv import load_dotenv
+from datetime import date
 
-from utils import create_slack_client, get_photo_dictionary_from_channel
+from utils import create_slack_client, get_photo_dictionary_from_channel, send_email
 
 load_dotenv()
 
@@ -10,6 +11,18 @@ load_dotenv()
 def handler(event, context):
     client = create_slack_client()
     photo_dictionary = get_photo_dictionary_from_channel("photos", client)
+
+    send_email(
+        send_from=os.environ["EMAIL_ADDRESS"],
+        send_to=[os.environ["PHOTO_FRAME_EMAIL_ADDRESS"]],
+        subject="New Photos From Slack",
+        text="Photos sent via AWS Lambda",
+        password=os.environ["EMAIL_PASSWORD"],
+        image_dict=photo_dictionary,
+        slack_token=os.environ["SLACK_BOT_TOKEN"],
+        server="smtp.gmail.com",
+    )
+
     body = {
         "message": photo_dictionary,
         "input": event,
--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
 import os
+import smtplib
+import requests
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
 
 from dotenv import load_dotenv
 from slack_sdk import WebClient
@@ -32,11 +38,51 @@ def get_photo_dictionary_from_channel(channel_name, client=None):
         if files is not None:
             for file in files:
                 try:
-                    photo_id = file.get("id")
+                    photo_name = file.get("name")
                     url_private = file.get("url_private")
-                    photo_dictionary.update({photo_id: url_private})
+                    photo_dictionary.update({photo_name: url_private})
                 except Exception as e:
                     print(e)
                     print(file)
 
     return photo_dictionary
+
+
+def send_email(
+    send_from,
+    send_to,
+    subject,
+    text,
+    password=None,
+    image_dict=None,
+    slack_token=None,
+    server="smtp.gmail.com",
+):
+    """
+    From https://stackoverflow.com/questions/3362600/how-to-send-email-attachments
+    """
+    assert isinstance(send_to, list)
+
+    msg = MIMEMultipart()
+    msg["From"] = send_from
+    msg["To"] = COMMASPACE.join(send_to)
+    msg["Date"] = formatdate(localtime=True)
+    msg["Subject"] = subject
+
+    msg.attach(MIMEText(text))
+
+    for image_name, image_url in image_dict.items():
+        if image_url is not None:
+            response = requests.get(
+                image_url, headers={"Authorization": "Bearer %s" % slack_token}
+            )
+            part = MIMEApplication(response.content, Name=image_name)
+            part["Content-Disposition"] = 'attachment; filename="%s"' % image_name
+            msg.attach(part)
+
+    smtp = smtplib.SMTP(server)
+    smtp.starttls()
+    smtp.login(send_from, password)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.close()
+
