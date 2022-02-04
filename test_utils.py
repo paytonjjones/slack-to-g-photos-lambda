429ca0cbf860fa15b5617408523d9cbfefb37493
Payton J. Jones <payton-jones@pluralsight.com>
2022-02-03 17:01:35 -0800

    add integration test for email client

--- test_utils.py
+++ test_utils.py
@@ -1,5 +1,9 @@
 import logging
+import os
+import smtplib
 from unittest.mock import Mock, patch
+
+import pytest
 from data.mocks import mock_message, mock_conversation_history
 from utils import (
     check_file_validity,
@@ -13,7 +17,8 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def test_create_slack_client__integration():
+@pytest.mark.integration
+def test_create_slack_client():
     client = create_slack_client()
     try:
         client.auth_test()
@@ -22,18 +27,36 @@ def test_create_slack_client__integration():
         assert False
 
 
-def test_get_channel_id__integration():
+@pytest.mark.integration
+def test_get_channel_id():
     client = create_slack_client()
     channel_id = get_channel_id("photos", client)
     assert channel_id == "C01AJKY3CKC"
 
 
+@pytest.mark.integration
+def test_google_access():
+    server = "smtp.gmail.com"
+    send_from = os.environ["EMAIL_ADDRESS"]
+    password = os.environ["EMAIL_PASSWORD"]
+    smtp = smtplib.SMTP(server, 587)
+    smtp.starttls()
+    try:
+        smtp.login(send_from, password)
+        assert True
+    except Exception as e:
+        logger.info(e)
+        assert False
+
+
+@pytest.mark.unit
 def test_get_formatted_image_name():
     file = mock_message["files"][0]
     image_name = get_formatted_image_name(file)
     assert image_name == "Image_from_iOS.jpg"
 
 
+@pytest.mark.unit
 def test_check_file_validity():
     mock_file_1 = {"mode": "tombstone", "url_private": "A Test URL"}
     assert not check_file_validity(mock_file_1)
@@ -59,6 +82,7 @@ def test_check_file_validity():
     assert check_file_validity(file)
 
 
+@pytest.mark.unit
 @patch("utils.get_channel_id")
 def test_get_photo_dictionary_from_channel(get_channel_id):
     client = Mock()
